9f58b48b2b52d3659cb3050193ee756910bc8bc1
jayohanna <33985435+jayohanna@users.noreply.github.com>
2018-01-27 16:44:59 -0500

    files
    files

---
+++ Scheduler/app/src/main/java/simple/android/scheduler/Invitations.java
@@ -0,0 +1,13 @@
+package simple.android.scheduler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Invitations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invitations);
+    }
+}
