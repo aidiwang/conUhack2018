fba977794f7878432732473bedce5b056aec0048
jayohanna <33985435+jayohanna@users.noreply.github.com>
2018-01-28 01:02:04 -0500

    log out to title

--- Scheduler/app/src/main/java/simple/android/scheduler/Settings.java
+++ Scheduler/app/src/main/java/simple/android/scheduler/Settings.java
@@ -8,14 +8,18 @@ import android.widget.Button;
 import android.widget.ImageButton;
 
 public class Settings extends AppCompatActivity {
-    Button create;
-    Button view;
-    Button invite;
-    ImageButton settings;
+    Button logoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        logoutButton=(Button)findViewById(R.id.logoutButton);
+    }
+
+    public void onClickLogOut(View v) {
+        Button logoutButton = (Button) v;
+        Intent i = new Intent(Settings.this, Title.class);
+        startActivity(i);
     }
 }
