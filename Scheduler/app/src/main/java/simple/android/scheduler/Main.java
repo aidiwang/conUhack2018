6ba001c65e0de11cb1c93a1373e695e603cf349c
jayohanna <33985435+jayohanna@users.noreply.github.com>
2018-01-27 23:24:50 -0500

    settings button

--- Scheduler/app/src/main/java/simple/android/scheduler/Main.java
+++ Scheduler/app/src/main/java/simple/android/scheduler/Main.java
@@ -5,11 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Main extends AppCompatActivity {
     Button create;
     Button view;
     Button invite;
+    ImageButton settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,24 +20,32 @@ public class Main extends AppCompatActivity {
         create=(Button)findViewById(R.id.create);
         view=(Button)findViewById(R.id.view);
         invite=(Button)findViewById(R.id.invite);
+        settings=(ImageButton)findViewById(R.id.settings);
     }
 
     public void onClick(View v) {
         Button create = (Button) v;
-        Intent i = new Intent(Main.this, Create.class);
+        Intent i = new Intent(Main.this, InviteContacts.class);
         startActivity(i);
     }
 
-    public void onClick2(View w) {
-        Button view = (Button) w;
-        Intent j = new Intent(Main.this, ViewEvents.class);
-        startActivity(j);
+    public void onClick2(View v) {
+        Button view = (Button) v;
+        Intent i = new Intent(Main.this, ViewEvents.class);
+        startActivity(i);
 
     }
 
-    public void onClick3(View x) {
-        Button invite = (Button) x;
-        Intent k = new Intent(Main.this, Invitations.class);
-        startActivity(k);
+    public void onClick3(View v) {
+        Button invite = (Button) v;
+        Intent i = new Intent(Main.this, Invitations.class);
+        startActivity(i);
     }
+
+    public void onClickSettings(View v) {
+        ImageButton settings = (ImageButton) v;
+        Intent i = new Intent(Main.this, Settings.class);
+        startActivity(i);
+    }
+
 }
